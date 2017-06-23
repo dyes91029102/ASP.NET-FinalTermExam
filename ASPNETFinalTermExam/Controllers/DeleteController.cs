b8078cdf302d946ec584f1fbbf0ca3e875410076
brook <brook@example.com>
2017-06-23 16:06:05 +0800

    查詢 刪除

---
+++ ASPNETFinalTermExam/Controllers/DeleteController.cs
@@ -0,0 +1,27 @@
+﻿using ASPNETFinalTermExam.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ASPNETFinalTermExam.Controllers
+{
+    public class DeleteController : Controller
+    {
+        // GET: Delete
+        [HttpPost()]
+        public ActionResult Delete(String CustomerID)
+        {
+
+            KuasDb db = new KuasDb();
+                //用where 那他出來的型態不一樣
+                Customers cus = db.Customers.Find(Int32.Parse(CustomerID));
+
+                db.Customers.Remove(cus);
+                db.SaveChanges();
+          
+            return View();
+        }
+    }
+}
--- ASPNETFinalTermExam/Controllers/SearchController.cs
+++ ASPNETFinalTermExam/Controllers/SearchController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ASPNETFinalTermExam.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,10 +9,45 @@ namespace ASPNETFinalTermExam.Controllers
 {
     public class SearchController : Controller
     {
+        KuasDb db = new KuasDb();
+        List<SelectListItem> ContactTitleList = new List<SelectListItem>();
         // GET: Search
         public ActionResult Search()
         {
+            GetData();
             return View();
+        }
+
+        [HttpPost()]
+        public JsonResult SearchAction(String CustomerID,String CustomerName,String ContactName,String ContactTitle)
+        {
+            var order = db.Customers.Where(x =>
+            (string.IsNullOrEmpty(CustomerID) ? true : x.CustomerID.ToString().Equals(CustomerID)) &&
+              (string.IsNullOrEmpty(CustomerName) ? true : x.CompanyName.ToString().Contains(CustomerName)) &&
+              (string.IsNullOrEmpty(ContactName) ? true : x.ContactName.ToString().Contains(ContactName)) &&
+              (string.IsNullOrEmpty(ContactTitle) ? true : x.ContactTitle.ToString().Equals(ContactTitle))
+            ).Select(x => new
+            {
+                x.CustomerID,
+                CustomerName=x.CompanyName,
+                x.ContactName,
+                x.ContactTitle
+            }
+            ).ToList();
+
+            return this.Json(order);
+        }
+
+
+        public void GetData()
+        {
+            ContactTitleList.Add(new SelectListItem
+            {
+                Text = "請選擇職稱",
+                Value = ""
+            });
+
+         
         }
     }
 }
